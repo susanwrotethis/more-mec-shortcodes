16f8ed9bd523635017a5d0a3dbfb4583505e7750
susanwrotethis <susanwrotethis@gmail.com>
2019-03-07 04:45:51 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,113 @@
+# More MEC Shortcodes
+
+This plugin was written for a small business offering series of classes for children, with each series normally running six or eight weeks. While its limited scope may not make it useful to other users of Modern Events Calendar, it provides a template that can be adapted for other custom uses.
+
+## Description
+
+Extends Modern Events Calendar Lite by adding a shortcode to display an events list in a non-caledar format. The shortcode is for the specific case of classes or activities posted as recurring events, one or more days a week over a series of several weeks.
+
+**Features**
+
+* Works on multisite as well as single installations
+* Clean installation: no options added to the database or settings page to manage
+* Contains language support
+
+## How It Works
+
+This plugin gets a list of all the event categories. For each category that has events, it displays the category name; the category description if one is available; and each event.
+
+The following fields are shown for events:
+
+* Event title
+* Day(s) of the week the event takes place
+* Event start and end times
+* Event location
+* Location address
+* Event cost
+* Number of weeks (calculated by the number of recurrences)
+* Start date of series
+* End date of series
+* Event link, displayed as a signup link
+
+The categories are listed in ascending order by category slug. The events are listed by menu order. Events with the same menu order are sorted by the date they were added. Both are in ascending order.
+
+## Limitations
+
+The shortcode will display all non-private published events assigned a category, even if the event has expired. To remove an event, delete it, make it private or set its status to draft or preview.
+
+A number of fields are not used in this format, including the event content, the excerpt, tags, event label, event color, featured image, organizers and the more info link.
+
+Single events, all-day events, the hiding of times or locations and hourly schedule information is ignored. It assumes that Repeat is set to Certain Weekdays and the After field is used to set when the event stops repeating.
+
+Categories cannot be excluded.
+
+There is only one layout format. While missing values for the fields that are used should not break the site, the results may not be pretty.
+
+## Installation
+
+Download the current release of this plugin as a zip file. Make sure the file is named more-mec-shortcodes.zip.
+
+* In the WordPress admin, go to Plugins > Add New. On multisite, this is under the network admin.
+* Click the Upload Plugin button at the top of the page and browse for the plugin's zip file.
+* Upload the zip file.
+* Once the plugin is installed, activate it. On multisite, this can be network activated or activated on individual sites.
+
+## Setup
+
+This plugin has no settings. It adds WordPress core's Menu Order setting to events to provide finer-grained control over the order of listings as they appear in the menu.
+
+If you have set up Modern Events Calendar Lite and added some recurring events, all you need to do is add the following shortcode to a page:
+
+[custom-event-list]
+
+## Frequently Asked Questions
+
+**Why is this plugin useful?**
+
+There are several good events calendar plugins available, but they focus on presenting events by date. But recurring events sometimes require a list of onging events or activites to be listed.
+
+The plugins that are configurable are more complicated than is needed for a small website. This plugin provides some of that functionality.
+
+**Why Modern Events Calendar?**
+
+Some of the events plugins provide recurring events only with the premium version. Others have events and recurring events set up separately. Modern Events Calendar Lite offers the recurring events with a single events interface and the right combination of taxonomies to be useful.
+
+**Will it work with other calendar plugins?**
+
+No, because the custom post type and taxonomy names are unique to Modern Events Calendar. A developer interested in adapting the concept of this shortcode could probably do so for other events plugins as long as they have comparable fields.
+
+**What happens if I deactivate or uninstall the calendar plugin?**
+
+If there are still published events in the website's database, this plugin will continue to display them. If there are no events found, this plugin won't do anything.
+
+**Will the menu order field affect the calendar's behavior elsewhere?**
+
+The field's addition should have no effect on Modern Events Calendar.
+
+**Does this plugin have a Gutenberg block?**
+
+Not at this time.
+
+**Is this plugin translation ready?**
+
+Yes.
+
+**Does it work on multisite?**
+
+Yes.
+
+**Is there anything else I should know?**
+
+This plugin was created to handle a very specific use case: there is a small number of events in the system (less than 50), they are all recurring events, and each is assigned a category and location.
+
+Because this plugin assembles and formats many different pieces of information, it is not ideal for large calendars.
+
+## About Modern Events Calendar
+
+See the Modern Events Calendar Lite plugin in [the WordPress plugins repository](https://wordpress.org/plugins/modern-events-calendar-lite/) to learn more.
+
+## Changelog
+
+### 0.1
+
+* New release
---
+++ more-mec-shortcodes.php
@@ -0,0 +1,240 @@
+<?php
+/*
+Plugin Name: More MEC Shortocdes
+Plugin URI: https://github.com/susanwrotethis/more-mec-shortcodes
+GitHub Plugin URI: https://github.com/susanwrotethis/more-mec-shortcodes
+Description: Extends Modern Events Calendar Lite by adding a shortcode to display an events list in a non-caledar format. The shortcode is for the specific case of classes or activities posted as recurring events, one or more days a week over a series of several weeks.
+Version: 1.0
+Author: Susan Walker
+Author URI: https://susanwrotethis.com
+License: GPL v2 or later
+Text Domain: swt-mec
+Domain Path: /lang/
+*/
+
+// Exit if loaded from outside of WP
+if ( !defined( 'ABSPATH' ) ) exit;
+
+// LANGUAGE SUPPORT SETUP BEGINS HERE ////////////////////////////////////////////////////
+// Load plugin textdomain
+function swt_mec_load_textdomain()
+{
+	load_plugin_textdomain( 'swt-mec', false, dirname( plugin_basename( __FILE__ ) ).'/lang/' );
+}
+add_action( 'plugins_loaded', 'swt_mec_load_textdomain' );
+
+// POST TYPE AND TAXONOMY EXTENSION FUNCTIONS BEGIN HERE /////////////////////////////////
+// Add page attributes to Modern Events Calendar events so menu order can be set.
+function swt_mec_menu_order() {
+	add_post_type_support( 'mec-events', 'page-attributes' );
+}
+add_action( 'init', 'swt_mec_menu_order' );
+
+// SHORTCODE CONTENT FORMATTING FUNCTIONS BEGIN HERE /////////////////////////////////////
+// Set string value of 'noon' or 'midnight' if the time in seconds matches.
+function swt_mec_set_noon_midnight( $seconds )
+{
+	switch( $seconds ) {
+		case 0:
+			return 'midnight';
+		case 43200:
+			return 'noon';
+		default:
+			return date( 'g:i a', $seconds );
+	}
+}
+
+// Format the start and end times of an event as a strng.
+function swt_mec_start_end_times( $event )
+{
+	$start = (int)$event->mec_start_day_seconds;
+	$end = (int)$event->mec_end_day_seconds;
+	
+	// Correct for midnight. This plugin sets the value in seconds at the end of the day.
+	$start = ( $start == 86400 ? 0 : $start );
+	$end = ( $end == 86400 ? 0 : $end );
+	
+	// Both times are after noon
+	if ( $start > 43200 && $end > 43200 ) {
+		return sprintf( date( 'g:i', $start ).'-'.date( 'g:i a', $end ), $start, $end );
+	}
+	
+	// Both times are between midnight and 11:59 am
+	else if ( $start > 0 && $start < 43200 && $end > 0 && $end < 43200 ) {
+		return sprintf( date( 'g:i', $start ).'-'.date( 'g:i a', $end ), $start, $end );
+	}
+	
+	// Mixed tmes with custom formatting
+	else {
+		return swt_mec_set_noon_midnight( $start ).'-'.swt_mec_set_noon_midnight( $end );
+	}
+}
+
+// Change integer to day of week
+function swt_mec_set_dow_name( &$item, $key )
+{
+	$dow = array( 
+		__( 'Sundays', 'swt-mec' ), 
+		__( 'Mondays', 'swt-mec' ), 
+		__( 'Tuesdays', 'swt-mec' ), 
+		__( 'Wednesdays', 'swt-mec' ), 
+		__( 'Thursdays', 'swt-mec' ), 
+		__( 'Fridays', 'swt-mec' ), 
+		__( 'Saturdays', 'swt-mec' ) 
+	);
+	$item = $dow[$item];
+}
+
+// Parse out the weekdays and return as string.
+function swt_mec_parse_days( $repeat )
+{
+	if ( !isset( $repeat['type'] ) || 'certain_weekdays' != $repeat['type'] ) {
+		return '';
+	}
+	
+	$days = $repeat['certain_weekdays'];
+	array_walk( $days, 'swt_mec_set_dow_name' );
+	
+	switch( count($days) ) {
+		
+		case 1:
+			return $days[0].', ';
+			
+		case 2:
+			return $days[0].' and '.$days[1].', ';
+			
+		default:
+			$last = array_pop( $days );
+			$all = implode( ', ', $days );
+			return $all.' and '.$last.', ';
+			
+	} // End switch
+}
+
+// Calculate end date for recurring sessions
+function swt_mec_format_recurring( $date, $repeat )
+{
+	$initial = strtotime( $date );
+
+	$start = date( 'F j', $initial );
+	$final = $initial+(($repeat-1)*604800); // add on # of weeks in seconds
+	$end = date( 'F j', $final );
+	
+	return '('.$start.'-'.$end.')';
+}
+
+// SHORTCODE FUNCTIONS BEGIN HERE ////////////////////////////////////////////////////////
+// Shortcode to list events by category.
+function swt_mec_list_events( $atts, $content=null )
+{
+	global $post;
+	
+	$o_loop = ''; // Outer loop contains full string to be returned
+	
+	// Get event category terms
+	$terms = get_terms( array(
+    	'taxonomy' => 'mec_category',
+    	'orderby' => 'slug',
+	) );
+	
+	// Exit if no terms found
+	if ( !$terms ) {
+		return '';
+	}
+
+	// Loop through terms
+	foreach( $terms as $term ):
+	
+		$term_id = $term->term_id;
+	
+		// Set up the query and get posts for current term
+		$args = array(
+			'post_type'			=> 'mec-events',
+			'tax_query' => array(
+    			array(
+					'taxonomy' => 'mec_category',
+					'field'    => 'term_id',
+					'terms'    => $term->term_id
+				) ),
+			'orderby'			=> 'menu_order',
+			'order'				=> 'ASC',
+		);
+		$events = get_posts( $args );
+		
+		// Output something for the term only if posts found
+		if ( $events ):
+		
+			// Concat term as header and description if found
+			$c_slug = $term->slug;
+			$section = '<h2 class="cat-'.$c_slug.'">'.trim( $term->name )."</h2>\n";
+		
+			if ( $desc = trim( term_description( $term_id ) ) ) { 
+				$section .= '<div class="session-cat-desc">'.$desc."</div>\n";
+			}
+			
+			$i_loop = ''; // Inner loop contains events by category
+		
+			// Loop through posts for term
+			foreach ( $events as $post ): 
+			
+				setup_postdata( $post );
+				
+				// Event name
+				$title = get_the_title();
+				$string = '<h3>'.$title.'</h3><div class="event-session in-'.$c_slug.'">';
+				
+				// Days of event
+				$repeat = maybe_unserialize( $post->mec_repeat );
+				$string .= '<div class="event-details"><p>'.swt_mec_parse_days( $repeat );
+				
+				// Start and end times of session
+				$string .= str_replace( ':00', '', swt_mec_start_end_times( $post ) ).', ';
+				
+				// Event location (a taxonomy term)
+				$location = get_term_by( 'id', $post->mec_location_id, 'mec_location' );
+				$string .= trim( $location->name );
+				
+				// Address (term meta)
+				if ( $address = get_term_meta( $location->term_id, 'address', true ) ) {
+					$string .= ', '.trim( $address );
+				}
+				$string .= '<br />';
+				
+				// Cost
+				$string .= trim( $post->mec_cost ).' for ';
+				
+				// Number of sessions for this class
+				$string .= $repeat['end_at_occurrences'].' weeks ';
+				
+				// concat the start and end dates
+				$start = trim( $post->mec_start_date );
+				$string .= swt_mec_format_recurring( $start, $repeat['end_at_occurrences'] );
+				
+				// Escape string built so far
+				//$string = esc_attr( $string );
+				$button_text = __( 'Sign Up', 'swt-mec' );
+				
+				// Signup URL with button class
+				$string .= '</p></div><div class="event-signup"><a class="gobutton"';
+				$string .= ' href="'.urlencode( trim( $post->mec_read_more ) ).'">';
+				$string .= $button_text.'<span class="screen-reader-text"> ';
+				$string .= sprintf( __('for %s', 'swt-mec' ), $title );
+				$string .= "</span></a></div></div>\n";
+             
+            	// Pass this iteration's string before resetting for next
+            	$i_loop .= $string;
+            	
+        	endforeach; // Next post
+        	
+        		// Concat section info and related iterations
+        		$o_loop .= $section.$i_loop;
+        	
+        	// Reset the query
+        	wp_reset_postdata();
+        	
+        endif; // End check for posts
+	endforeach; // Next term
+	
+	return $o_loop;
+}
+add_shortcode( 'custom-event-list', 'swt_mec_list_events');
